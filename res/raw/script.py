7b08d22cac63db7d5612eb4159086082e9c3f757
raul <raul@001aeff7-3401-0410-a489-f7902fc005dd>
2012-02-24 09:22:51 +0000

    added dht-swift interface code
    git-svn-id: http://svn.tribler.org/abc/branches/droid-release-0.1@25100 001aeff7-3401-0410-a489-f7902fc005dd

--- res/raw/script.py
+++ res/raw/script.py
@@ -1,9 +1,12 @@
 import android,time
+import dht
+
 droid = android.Android()
 droid.makeToast('Python says: Hello, Android!')
 droid.vibrate(300)
 
 
-droid.makeToast("Python says: Done")  
 
-
+droid.makeToast("Starting Mock DHT ...")  
+dht.SwiftTraker(None, 9999)
+droid.makeToast("Mock DHT running!")  
