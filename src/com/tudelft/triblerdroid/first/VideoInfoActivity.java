947e4830009c7b0e9061185fa33a064d44309cae
Raul Jimenez <rauljc@kth.se>
2012-03-08 13:37:12 +0100

    VideoInfoActivity as MAIN and ScriptActivity working

---
+++ src/com/tudelft/triblerdroid/first/VideoInfoActivity.java
@@ -0,0 +1,77 @@
+package com.tudelft.triblerdroid.first;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+public class VideoInfoActivity extends Activity {
+	
+ 	String title = "TT";
+	String description = "DD";
+	String url;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.video_info);
+    	
+    	//Bundle extras = getIntent().getExtras();
+    	final Integer pos = 0;//extras.getInt("video_pos", 0);
+    	
+    	
+    	switch (pos) {
+    		case 0: 
+    			title =  (String) getResources().getText(R.string.v1_title);
+    			description =  (String) getResources().getText(R.string.v1_description);
+    			url =  (String) getResources().getText(R.string.v1_url);
+        		break;
+    		case 1:
+    			title =  (String) getResources().getText(R.string.v2_title);
+    			description =  (String) getResources().getText(R.string.v2_description);
+    			url =  (String) getResources().getText(R.string.v2_url);
+    			break;
+    		case 2:
+    			title =  (String) getResources().getText(R.string.v3_title);
+    			description =  (String) getResources().getText(R.string.v3_description);
+    			url =  (String) getResources().getText(R.string.v3_url);
+    			break;
+    	}
+    	
+    	TextView t = (TextView) findViewById(R.id.title);
+    	t.setText(title);
+    	t = (TextView) findViewById(R.id.description);
+    	t.setText(description);
+
+    	Button b_play = (Button) findViewById(R.id.b_play);
+  	  
+    	b_play.setOnClickListener(new OnClickListener() {
+      	    public void onClick(View view) {
+      	    	//Start video
+//      	    	Intent intent = new Intent(getBaseContext(), VideoActivity.class);
+//    	    	intent.putExtra("video_pos", pos);
+      	    	//FIXME
+      	    	Intent intent = new Intent(getBaseContext(), ScriptActivity.class);
+      	    	startActivity(intent);
+      	    }
+      	});
+    	
+      	Button b_web_info = (Button) findViewById(R.id.b_web_info);
+      	 	  
+      	b_web_info.setOnClickListener(new OnClickListener() {
+      	    public void onClick(View view) {
+      	    	//Start video
+      	    	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+      	      	startActivity(intent);
+      	    }
+      	});
+    }
+      
+}
