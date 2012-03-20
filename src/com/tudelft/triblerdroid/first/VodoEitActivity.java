2278c0d4b5fe785a6753c5833bb5570aa4de3783
Arno Bakker <arno@cs.vu.nl>
2012-03-20 15:50:52 +0100

    - Added hashes and set tracker to tracker3.p2p-next.org:20050

--- src/com/tudelft/triblerdroid/first/VodoEitActivity.java
+++ src/com/tudelft/triblerdroid/first/VodoEitActivity.java
@@ -51,7 +51,7 @@ public class VodoEitActivity extends ListActivity {
 //    	    	Play video
     	    	Intent intent = new Intent(getBaseContext(), ScriptActivity.class);
     	    	intent.putExtra("hash", HASHES[position]);
-    	    	intent.putExtra("tracker", "tracker3.p2p-next.org:2002");
+    	    	intent.putExtra("tracker", "tracker3.p2p-next.org:20050");
 //    	    	intent.putExtra("destination", destination);
       	    	startActivity(intent);
 
@@ -60,7 +60,7 @@ public class VodoEitActivity extends ListActivity {
     	      
     	    }
     	  });
-    	}
+    	} // Arno: If you change the order here, change HASHES[] order as well!
 	static final String[] VIDEOS = new String[] {
 		"Ken Robinson says schools kill creativity", 
 		"Jill Bolte Taylor's stroke of insight", 
@@ -71,7 +71,7 @@ public class VodoEitActivity extends ListActivity {
 		"Arthur Benjamin does 'Mathemagic'", 
 		"Hans Rosling shows the best stats you've ever seen", 
 		"Rob Reid: The $8 billion iPod", 
-		"Brené Brown: Listening to shame", 
+		"Brene Brown: Listening to shame", 
     	"Susan Cain: The power of introverts", 
 		"Vijay Kumar: Robots that fly ... and cooperate", 
     	"I Think Were Alone Now", 
@@ -80,23 +80,23 @@ public class VodoEitActivity extends ListActivity {
 		"An Honest Man", 
     };
 	static final String[] HASHES = new String[] {
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-		"280244b5e0f22b167f96c08605ee879b0274ce22", 
-    };
+		"2b2fe5f1462e5b7ac4d70fa081e0169160b2d3a6", // SirKenRobinson_2006-480p.ts
+		"a004e583a05de39f87ceb7a6eb5608c89415e2f0", // JillBolteTaylor_2008-480p.ts
+		"23f99be0f5198efceb4da15fd196106b70216e1e", // PranavMistry_2009I-480p.ts
+		"022e1c308d991c653c5e3549fee247cfabc7cf55", // DavidGallo_2007-480p.ts
+		"5e615dfdf66953f63be284ae0763f80cb70f0892", // PattieMaes_2009-480p.ts
+		"e5478e34e01551a2925fc12f4d28a523b2911af5", // SimonSinek_2009X-480p.ts
+		"ec677ff98abe4a0b2b5c122065c080f14ad4a272", // ArthurBenjamin_2005-480p.ts
+		"71ccb9341537a9a5738650e6842c97fc88306582", // HansRosling_2006.ts
+		"ad2fa2dd346f67583ab327a14d739bdccd44cdb3", // RobReid_2012-480p.ts
+		"2dcb65253916e44a791ac7a1a0ee56f51f30086f", // BreneBrown_2012-480p.ts
+		"5692014fadcdb33792f0cfa7cc87287bfb8deb91", // SusanCain_2012-480p.ts
+		"dbfcbf3e5ca676d1e4ea8a88375ea95ce2f2184f", // VijayKumar_2012-480p.ts
+		"db5dabb90a3cbd61a90866a4cc208ae959440ec9", // I.Think.Were.Alone.Now.2008.720p.x264-VODO.ts
+		"071d43828a3291defa073008b601aacfb09fd281", // L5.Part.1.2012.Xvid-VODO.ts
+		"cbc48a70222e37230bf3f2b3bd84eaef5ae16b41", // Pioneer.One.S01E06.Xvid-VODO.ts
+		"3ce3f4a5bb785d5e8eb7bf3f2615e37095eb5170", // An.Honest.Man.Xvid-VODO.ts
+	};
       
 }
 
