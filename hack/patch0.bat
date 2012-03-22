8ef3d0ebb89cb93fd1eac5c185ec29910892d3b5
Arno Bakker <arno@cs.vu.nl>
2012-03-22 11:16:06 +0100

    - Typo and Win32 build fix.

--- hack/patch0.bat
+++ hack/patch0.bat
@@ -1,6 +1,6 @@
-c:\Users\arno\AppData\Local\Android\android-sdk\platform-tools\aapt.exe a ..\bin\TriblerDroid-release-0.1.apk lib/armeabi/libcom_googlecode_android_scripting_Exec.so
+c:\Users\arno\AppData\Local\Android\android-sdk\platform-tools\aapt.exe a ..\bin\tgs-android.apk lib/armeabi/libcom_googlecode_android_scripting_Exec.so
 
 REM Must be 1.6.0_24+, not 1.7, see
 REM http://code.google.com/p/android/issues/detail?id=830
-"c:\Program Files\Java\jdk1.6.0_26\bin\jarsigner" -verbose -keystore c:\users\arno\.android\debug.keystore -storepass android -keypass android -digestalg SHA1 -sigalg MD5withRSA -sigfile CERT -signedjar HelloWorld2.apk ..\bin\TriblerDroid-release-0.1.apk androiddebugkey
-copy HelloWorld2.apk ..\bin\TriblerDroid-release-0.1.apk
+"c:\Program Files\Java\jdk1.6.0_26\bin\jarsigner" -verbose -keystore c:\users\arno\.android\debug.keystore -storepass android -keypass android -digestalg SHA1 -sigalg MD5withRSA -sigfile CERT -signedjar HelloWorld2.apk ..\bin\tgs-android.apk androiddebugkey
+copy HelloWorld2.apk ..\bin\tgs-android.apk
