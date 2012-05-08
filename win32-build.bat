e26ade634b26fc300ef8fdd92b6c4d3fbf8b3f3a
unknown <tribler@winxp-dev.(none)>
2012-05-08 16:06:34 +0200

    - JNI automatic build (once jni/ link has been updated)

--- win32-build.bat
+++ win32-build.bat
@@ -1,4 +1,11 @@
 set JAVA_HOME=c:\Progra~1\Java\jdk1.7.0_04
-\build\apache-ant-1.8.3\bin\ant release
 
-CALL hack\patch2.bat
+cd jni
+CALL win32-build-jni.bat
+cd ..
+
+CALL \build\apache-ant-1.8.3\bin\ant release
+
+cd hack
+CALL patch2.bat
+cd ..
